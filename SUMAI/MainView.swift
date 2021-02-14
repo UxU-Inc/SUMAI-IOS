42bde0cc39d4bd9b3da86d8a10c0cf7ffbed7cf7
tkarlz <tjdudrb97@gmail.com>
2021-02-15 01:11:02 +0900

    Apply .gitignore

--- SUMAI/MainView.swift
+++ SUMAI/MainView.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var data: String = ""
+    @State private var summary: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack{
+                ScrollView(){
+                    TextField("요약할 내용을 입력하세요.", text: $data)
+                        .frame(height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .padding()
+                        .background(Color.white)
+                    TextField("", text: $summary)
+                        .background(Color.white)
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar{
+                ToolbarItem(placement: .navigationBarLeading){
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "line.horizontal.3")
+                            .font(.title)
+                    })
+                }
+                ToolbarItem(placement: .navigationBarLeading){
+                    HStack(spacing: 8){
+                        Image("SUMAILOGO")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 90, height: 33)
+                        Text("요약")
+                            .font(.title)
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "person.circle")
+                            .font(.title2)
+                    })
+                }
+            }
+        }
+        .accentColor(.primary)
     }
 }
 
