33d29e6aa24e331e4cabc42ca9338e6003fb9a22
tkarlz <tjdudrb97@gmail.com>
2021-10-12 15:06:01 +0900

    Modify sensitive data

--- SUMAI/APIComm.swift
+++ SUMAI/APIComm.swift
@@ -2,7 +2,61 @@
 //  APIComm.swift
 //  SUMAI
 //
-//  Created by 서영규 on 2021/10/12.
+//  Created by 서영규 on 2021/02/19.
 //
-
 import Foundation
+
+struct Param : Codable{
+    let data : String
+    let id : String
+    let record : Bool
+}
+
+struct Summary : Decodable {
+    let summarize: String
+}
+
+class APIComm : ObservableObject {
+    @Published var loading : Bool = false
+    
+    func requestSummary(data: String, completion: @escaping (String) -> ()) {
+        self.loading.toggle()
+        let id : String = ""
+        let record : Bool = false
+        
+        let param = Param(data: data, id: id, record: record)
+        let paramData = try! JSONEncoder().encode(param)
+        //print(String(data: paramData, encoding: .utf8) ?? "")
+        
+        let url = URL(string: APIData.summaryUrl)!
+        
+        var request = URLRequest(url: url, timeoutInterval: 60.0)
+        request.httpMethod = "POST"
+        request.httpBody = paramData
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if error != nil {
+                DispatchQueue.main.async {
+                    self.loading.toggle()
+                }
+                //print("error1: \(e.localizedDescription)")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                let object = try? JSONDecoder().decode(Summary.self, from: data!)
+                if let jsonObject = object {
+                    let summarize = jsonObject.summarize
+                    
+                    //print(summarize)
+                    completion(summarize)
+                }
+                self.loading.toggle()
+            }
+        }
+        
+        task.resume()
+    }
+}
--- SUMAI/AdmobView.swift
+++ SUMAI/AdmobView.swift
@@ -1,8 +1,67 @@
 //
-//  AdmobView.swift
+//  GADBannerViewController.swift
 //  SUMAI
 //
-//  Created by 서영규 on 2021/10/12.
+//  Created by 서영규 on 2021/02/22.
 //
+import SwiftUI
+import GoogleMobileAds
 
-import Foundation
+struct GADBannerViewController: UIViewControllerRepresentable {
+
+    @State private var banner: GADBannerView = GADBannerView(adSize: kGADAdSizeBanner)
+
+    func makeUIViewController(context: Context) -> UIViewController {
+        let bannerSize = GADBannerViewController.getAdBannerSize()
+        let viewController = UIViewController()
+        banner.adSize = bannerSize
+        banner.adUnitID = AdmobData.bannerId
+        banner.rootViewController = viewController
+        viewController.view.addSubview(banner)
+        viewController.view.frame = CGRect(origin: .zero, size: bannerSize.size)
+        banner.load(GADRequest())
+        return viewController
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context){
+        let bannerSize = GADBannerViewController.getAdBannerSize()
+        banner.frame = CGRect(origin: .zero, size: bannerSize.size)
+        banner.load(GADRequest())
+    }
+
+    static func getAdBannerSize() -> GADAdSize {
+        if let rootView = UIApplication.shared.windows.first?.rootViewController?.view {
+            let frame = rootView.frame.inset(by: rootView.safeAreaInsets)
+            return GADCurrentOrientationAnchoredAdaptiveBannerAdSizeWithWidth(frame.width)
+        }
+        //No root VC, use 320x50 ad banner
+        return kGADAdSizeBanner
+    }
+}
+
+final class GADInterstitialController : NSObject, GADInterstitialDelegate {
+    var interstitial : GADInterstitial!
+    
+    override init() {
+        super.init()
+        loadInterstitial()
+    }
+    
+    func loadInterstitial() {
+        self.interstitial = GADInterstitial(adUnitID: AdmobData.interstitialId)
+        self.interstitial.load(GADRequest())
+        self.interstitial.delegate = self
+    }
+    
+    func showAd() {
+        if self.interstitial.isReady{
+            let root = UIApplication.shared.windows.first?.rootViewController
+            self.interstitial.present(fromRootViewController: root!)
+        }
+    }
+    
+    func interstitialDidDismissScreen(_ ad: GADInterstitial) {
+        self.loadInterstitial()
+    }
+    
+}
